862673b8b5145b8733ad1fb58345c0009839774f
Semmy Purewal <semmypurewal@gmail.com>
2013-04-05 09:28:12 -0400

    update to latest version of node-dev-bootstrap

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,16 @@
 Vagrant::Config.run do |config|
-  config.vm.box = "base"
+  config.vm.box = "precise32"
+  
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
 
   config.vm.forward_port 3000, 3000
 
-  config.vm.share_folder "examples", "/home/vagrant/examples", "examples"
+  config.vm.share_folder "examples", "/home/vagrant/app", "examples"
+
+  # Uncomment the following line to allow for symlinks
+  # in the app folder. This will not work on Windows, and will
+  # not work with Vagrant providers other than VirtualBox
+  # config.vm.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/app", "1"]
 
   config.vm.provision :chef_solo do |chef|
     chef.add_recipe "nodejs"
@@ -11,8 +18,12 @@ Vagrant::Config.run do |config|
     chef.add_recipe "redis-server"
     chef.json = {
       "nodejs" => {
-        "version" => "0.6.12"
-      }    	      
+        "version" => "0.10.0"
+        # uncomment the following line to force
+	# recent versions (> 0.8.5) to be built from
+	# the source code
+	# , "from_source" => true
+      }
     }
   end
 end
--- cookbooks/nodejs/recipes/default.rb
+++ cookbooks/nodejs/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Author:: Marius Ducea (marius@promethost.com)
+# Author:: Marius Ducea (marius@promethost.com) and Semmy Purewal (semmypurewal@gmail.com)
 # Cookbook Name:: nodejs
 # Recipe:: default
 #
@@ -18,29 +18,77 @@
 # limitations under the License.
 #
 
-include_recipe "build-essential"
 
-case node[:platform]
-  when "centos","redhat","fedora"
-    package "openssl-devel"
-  when "debian","ubuntu"
-    package "libssl-dev"
-end
+# split up teh version to extract integers
+# there's probably a better way to do this
+version_array = node[:nodejs][:version].split(".").map { |x| x.to_i };
 
-path = node[:nodejs][:version]>"0.5.0"?
+# get the path for the appropriate version
+path = version_array[1] > 5 ||
+       version_array[1] == 5 && version_array[2] >= 1?
             "http://nodejs.org/dist/v#{node[:nodejs][:version]}/":
             "http://nodejs.org/dist/"
 
-bash "install nodejs from source" do
-  cwd "/usr/local/src"
-  user "root"
-  code <<-EOH
-    wget #{path}node-v#{node[:nodejs][:version]}.tar.gz && \
-    tar zxf node-v#{node[:nodejs][:version]}.tar.gz && \
-    cd node-v#{node[:nodejs][:version]} && \
-    ./configure --prefix=#{node[:nodejs][:dir]} && \
-    make && \
-    make install
-  EOH
-  not_if "#{node[:nodejs][:dir]}/bin/node -v 2>&1 | grep 'v#{node[:nodejs][:version]}'"
+from_source = (node[:nodejs] != nil && \
+               node[:nodejs][:from_source] != nil && \
+               node[:nodejs][:from_source]) || \
+               version_array[1] < 8 || \
+               version_array[1] == 8 && version_array[2] < 6
+
+if from_source == true
+  include_recipe "build-essential"
+
+  case node[:platform]
+    when "centos","redhat","fedora"
+      package "openssl-devel"
+    when "debian","ubuntu"
+      package "libssl-dev"
+  end
+
+  # install node.js from source
+  bash "install nodejs from source" do
+    cwd "/usr/local/src"
+    user "root"
+    code <<-EOH
+      wget #{path}node-v#{node[:nodejs][:version]}.tar.gz && \
+      tar zxf node-v#{node[:nodejs][:version]}.tar.gz && \
+      cd node-v#{node[:nodejs][:version]} && \
+      ./configure --prefix=#{node[:nodejs][:dir]} && \
+      make && \
+      make install
+    EOH
+    not_if "#{node[:nodejs][:dir]}/bin/node -v 2>&1 | grep 'v#{node[:nodejs][:version]}'"
+  end
+
+  # install npm if the version is less than 0.6.3
+  if node[:nodejs][:version] < "0.6.3"
+    package "curl"
+
+    bash "install npm" do
+      cwd "/usr/local/src"
+      user "root"
+      cwd "/tmp/"	
+      code <<-EOH
+        curl https://npmjs.org/install.sh | clean=no sh    
+      EOH
+      not_if "#{node[:nodejs][:dir]}/bin/npm -v 2>&1 | grep '#{node[:nodejs][:npm]}'"
+    end
+  end
+
+else
+  # install node.js from the binary
+  bash "install nodejs binary" do
+    cwd "/opt"
+    user "root"
+    code <<-EOH
+      wget #{path}node-v#{node[:nodejs][:version]}-linux-x86.tar.gz && \
+      tar zxf node-v#{node[:nodejs][:version]}-linux-x86.tar.gz && \
+      ln -s /opt/node-v#{node[:nodejs][:version]}-linux-x86/bin/node /usr/local/bin/node && \
+      ln -s /opt/node-v#{node[:nodejs][:version]}-linux-x86/bin/node-waf /usr/local/bin/node-waf && \
+      ln -s /opt/node-v#{node[:nodejs][:version]}-linux-x86/bin/npm /usr/local/bin/npm
+    EOH
+    not_if "/opt/node-v#{node[:nodejs][:version]}-linux-x86/bin/node -v | grep 'v#{node[:nodejs][:version]}'"
+  end
+
+
 end
